f714bb3a716137ae05240c14bb71bb40e5f8019d
leeminseok22 <cube9072@naver.com>
2022-04-28 20:23:14 +0900

    to do 체크리스트 추가했습니다.

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template, request, jsonify, redirect
-from pymongo import MongoClient
-import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
 from calendar import monthrange
 
-client = MongoClient('mongodb+srv://test:sparta@cluster0.q4meh.mongodb.net/cluster0?retryWrites=true&w=majority')
+from pymongo import MongoClient
+import certifi
+
+ca = certifi.where()
+
+# mongodb+srv://test:sparta@cluster0.q4meh.mongodb.net/cluster0?retryWrites=true&w=majority
+client = MongoClient('mongodb+srv://test:sparta@cluster0.q4meh.mongodb.net/cluster0?retryWrites=true&w=majority',tlsCAFile=ca)
 db = client.dbsparta
 app = Flask(__name__)
 
@@ -71,6 +75,38 @@ def culture_get():
         count += 1
 
     return jsonify(row)
+
+
+@app.route("/bucket", methods=["POST"])
+def bucket_post():
+    bucket_receive = request.form['bucket_give']
+
+    bucket_list = list(db.bucket.find({}, {'_id': False}))
+    count = len(bucket_list) + 1
+
+    doc = {
+        'num':count,
+        'bucket':bucket_receive,
+        'done':0
+    }
+    db.bucket.insert_one(doc)
+
+
+    return jsonify({'msg': '등록완료!'})
+
+@app.route("/bucket/done", methods=["POST"])
+def bucket_done():
+    num_receive = request.form['num_give']
+    db.bucket.update_one({'num':int(num_receive)}, {'$set': {'done': 1}})
+    return jsonify({'msg': 'bucket complete'})
+
+@app.route("/bucket", methods=["GET"])
+def bucket_get():
+
+    bucket_list = list(db.bucket.find({}, {'_id': False}))
+
+    return jsonify({'buckets': bucket_list})
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
